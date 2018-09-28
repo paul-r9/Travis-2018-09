4ea58b040de0bc0a31f5e9a254de336f0c9a5796
Cristina Hanes <cristina8bit@gmail.com>
2018-09-28 10:32:12 -0700

    wrote one test, have it working.

--- java/ttt_3/src/main/java/TicTacToe.java
+++ java/ttt_3/src/main/java/TicTacToe.java
@@ -1,6 +1,13 @@
+import java.lang.reflect.Array;
+
 public class TicTacToe {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+
+    public String getBoard() {
+        return "";
     }
 }
