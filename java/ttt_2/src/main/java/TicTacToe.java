ecf9ac03873f2c1b72072defcd1be50cbfb5452c
Josh DeSherlia <josh.desherlia@wdc.com>
2018-09-28 10:43:07 -0700

    Checking that there are 2 players on game start

--- java/ttt_2/src/main/java/TicTacToe.java
+++ java/ttt_2/src/main/java/TicTacToe.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicTacToe {
+
+    public static final int NUM_PLAYERS = 2;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static int startGame() {
+
+        List<PlayerInstance> players = new ArrayList<>();
+
+        for (int p = 0; p < NUM_PLAYERS; p++) {
+            PlayerInstance player = new PlayerInstance();
+            players.add(player);
+        }
+
+        return players.size();
     }
 }
--- java/ttt_2/src/test/java/SampleTest.java
+++ java/ttt_2/src/test/java/SampleTest.java
@@ -3,12 +3,24 @@ import org.junit.Test;
 
 public class SampleTest {
 
+    //    There are 2 players
+    @Test
+    public void onGameStartThereAreTwoPlayers() {
+
+        // Arrange
+        // Not Sure
+
+        // Act
+        int actual = TicTacToe.startGame();
+
+        // Assert
+        Assert.assertEquals(2, actual);
+    }
+
     //    Players can't go twice
     //    One Piece per space
-    //
-    @Test
-    public void exampleTest() {
-        Assert.assertTrue("Example of a passing assertion", true);
-//        Assert.fail("break travis");
-    }
+    //    Game over when all fields in a column are taken
+    //    Game over when all fields in a row are taken
+    //    Game over when all fields in a diagonal are taken
+    //    Game over when all fields are taken
 }
