9eb87e333b831fc0b8167927a24f5ce18430c486
mikhail <mikhail.j.nguyen@gmail.com>
2018-09-28 10:16:47 -0700

    Test for instantiating board

--- java/ttt_1/src/main/java/TicTacToe.java
+++ java/ttt_1/src/main/java/TicTacToe.java
@@ -1,5 +1,7 @@
 public class TicTacToe {
 
+    public static String[][] board = new String[3][3];
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
